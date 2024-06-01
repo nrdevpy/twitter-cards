b21dfcb583b0622f850d5041ea88eee9bc04d5a3
NrDev <nelson_rojas_janda@hotmail.com>
2024-06-01 14:15:37 +0200

    Add TwitterFollowCards component

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,11 @@
-import { TwitterFollowCard } from "./components/TwitterFollowCard"
-import { TwitterCards } from "./type"
-
-const users: TwitterCards = [
-  {
-    userName: "n3lsonroj4s",
-    name: "Nelson Rojas Janda",
-    isFollowing: false,
-  },
-  {
-    userName: "EvilAFM",
-    name: "Alexelcapo",
-    isFollowing: false,
-  },
-  {
-    userName: "midudev",
-    name: "Miguel Angel Durán",
-    isFollowing: false,
-  }
-]
+import { TwitterFollowCards } from "./components/TwitterFollowCards"
 
 function App() {
   return (
     <div className="min-w-screen min-h-screen">
       <h1 className="text-3xl font-bold text-center py-12">Twitter Cards</h1>
       <section className="flex flex-wrap justify-center gap-8">
-        {users.map((user) => {
-          const { userName, name, isFollowing } = user
-          return (
-            <TwitterFollowCard userName={userName} name={name} isFollowing={isFollowing} />
-          )
-        })}
+        <TwitterFollowCards />
       </section>
     </div>
   )
