b3cc9fa967f097f96aa424169d3b01d421283966
NrDev <nelson_rojas_janda@hotmail.com>
2024-06-02 14:04:59 +0200

    Add follow button state

--- src/components/TwitterFollowCard.tsx
+++ src/components/TwitterFollowCard.tsx
@@ -1,9 +1,16 @@
 import { TwitterCard } from "../type"
+import { useState } from "react"
 
 type Props = TwitterCard
 
 export const TwitterFollowCard: React.FC<Props> = ({ userName, name, isFollowing }) => {
+    const [followStatus, setFollowStatus] = useState(isFollowing)
     const atUserName = `@${userName}`
+
+    const btnClassName = followStatus
+        ? "bg-transparent hover:bg-[#200a0b] hover:text-[#e71f2b] hover:border-[#e71f2b]"
+        : "bg-white text-black hover:bg-gray-100 hover:border-gray-100 hover:border-gray-100"
+
     return (
         <li className="flex items-center justify-between max-w-xs py-1 px-4 min-w-[22rem]">
             <header className="flex items-center justify-center gap-2">
@@ -18,7 +25,12 @@ export const TwitterFollowCard: React.FC<Props> = ({ userName, name, isFollowing
                 </div>
             </header>
             <aside>
-                <button className="bg-white text-black font-medium py-1 px-3 rounded-full">{isFollowing ? "Unfollow" : "Follow"}</button>
+                <button
+                    className={`${btnClassName} font-medium py-1 px-3 rounded-full border-[.6px] border-white`}
+                    onClick={() => setFollowStatus(!followStatus)}
+                >
+                    {followStatus ? "Unfollow" : "Follow"}
+                </button>
             </aside>
         </li>
     )
