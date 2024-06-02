b13637877f4a76b913c469b50b704a6b15a82353
NrDev <nelson_rojas_janda@hotmail.com>
2024-06-02 15:06:00 +0200

    Fix button hover styles on mobile

--- src/components/TwitterFollowButton.tsx
+++ src/components/TwitterFollowButton.tsx
@@ -5,8 +5,8 @@ import { TwitterIsFollowingType } from "../type"
 export const TwitterFollowButton: React.FC<TwitterIsFollowingType> = ({ isFollowing }) => {
     const [followStatus, setFollowStatus] = useState(isFollowing)
     const btnClassName = followStatus
-        ? "bg-transparent hover:bg-[#200a0b] hover:text-[#e71f2b] hover:border-[#e71f2b]"
-        : "bg-white text-black hover:bg-gray-100 hover:border-gray-100 hover:border-gray-100"
+        ? "bg-transparent md:hover:bg-[#200a0b] md:hover:text-[#e71f2b] md:hover:border-[#e71f2b]"
+        : "bg-white text-black md:hover:bg-gray-100 md:hover:border-gray-100 md:hover:border-gray-100"
 
     return (
         <button
