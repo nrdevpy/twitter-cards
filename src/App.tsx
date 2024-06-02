50c327453c211059a800570bb109a0d0e4edcf7a
NrDev <nelson_rojas_janda@hotmail.com>
2024-06-02 14:48:34 +0200

    Add more users and style the list of users

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,9 @@ import { TwitterFollowCards } from "./components/TwitterFollowCards"
 
 function App() {
   return (
-    <div className="min-w-screen min-h-screen">
-      <h1 className="text-3xl font-bold text-center py-12">Twitter Cards</h1>
-      <section className="flex flex-wrap justify-center gap-8">
+    <div className="min-w-screen min-h-screen relative">
+      <h1 className="text-3xl font-bold text-center py-12 fixed min-w-full bg-gray-800">Twitter Cards</h1>
+      <section className="flex flex-wrap justify-center py-36">
         <TwitterFollowCards />
       </section>
     </div>
--- src/components/TwitterFollowCards.tsx
+++ src/components/TwitterFollowCards.tsx
@@ -5,26 +5,74 @@ const users: Users = [
     {
         id: crypto.randomUUID(),
         userName: "n3lsonroj4s",
-        name: "Nelson Rojas Janda",
+        name: "Nelson Rojas",
+        isFollowing: true,
+    },
+    {
+        id: crypto.randomUUID(),
+        userName: "paola_montero07",
+        name: "Paola Montero",
         isFollowing: true,
     },
     {
         id: crypto.randomUUID(),
         userName: "EvilAFM",
         name: "Alexelcapo",
+        isFollowing: true,
+    },
+    {
+        id: crypto.randomUUID(),
+        userName: "MoureDev",
+        name: "Brais Moure",
+        isFollowing: true,
+    },
+    {
+        id: crypto.randomUUID(),
+        userName: "kendokaponimami",
+        name: "FrascoDeVeneno",
         isFollowing: false,
     },
     {
         id: crypto.randomUUID(),
         userName: "midudev",
         name: "Miguel Angel Durán",
+        isFollowing: true,
+    },
+    {
+        id: crypto.randomUUID(),
+        userName: "IbaiLlanos",
+        name: "Ibai",
         isFollowing: false,
-    }
+    },
+    {
+        id: crypto.randomUUID(),
+        userName: "MADLionsKOI",
+        name: "MAD Lions KOI",
+        isFollowing: true,
+    },
+    {
+        id: crypto.randomUUID(),
+        userName: "CORSAIR",
+        name: "CORSAIR",
+        isFollowing: false,
+    },
+    {
+        id: crypto.randomUUID(),
+        userName: "AMD",
+        name: "AMD",
+        isFollowing: true,
+    },
+    {
+        id: crypto.randomUUID(),
+        userName: "_nasch_",
+        name: "Nicolás Schürmann",
+        isFollowing: false,
+    },
 ]
 
 export const TwitterFollowCards = () => {
     return (
-        <ul className="flex flex-wrap justify-center gap-8">
+        <ul className="flex flex-col justify-center gap-8">
             {users.map((user) => {
                 const { userName, name, isFollowing, id } = user
                 return (
